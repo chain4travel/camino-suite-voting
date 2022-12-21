c7d53e55f443d30bf89e4acfa1d0eced22747e7e
jamesckhsiao <james.h@cryptophet.com>
2022-12-21 17:26:22 +0800

    fix(router): modify default route to vote

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,27 @@
-import React from "react";
-import { createBrowserRouter } from "react-router-dom";
+import React from 'react';
+import { createBrowserRouter, redirect } from 'react-router-dom';
 
-import Vote from "../pages/Vote";
-import { CreateProposal, ProposalList } from "../pages/Proposals";
+import Vote from '../pages/Vote';
+import { CreateProposal, ProposalList } from '../pages/Proposals';
 
 export const routes = [
   {
-    path: "/vote",
-    element: <Vote />,
-    index: true
+    path: '/',
+    loader: () => redirect('/vote'),
   },
   {
-    path: "/vote/:category",
+    path: '/vote',
+    element: <Vote />,
+  },
+  {
+    path: '/vote/:category',
     element: <ProposalList />,
   },
   {
-    path: "/vote/:category/create",
+    path: '/vote/:category/create',
     element: <CreateProposal />,
   },
-]
-const router = createBrowserRouter(routes)
+];
+const router = createBrowserRouter(routes);
 
 export default router;
