207a91476ff5452ff538f1461a9e335e3b2bd6fb
Jameshsiao <heroms90@gmail.com>
2023-07-03 13:46:58 +0800

    refactor(common components): modify default styles of common components
    includes,
    - Button
    - RadioButton
    - DistributionBar

--- src/components/DistributionBar.tsx
+++ src/components/DistributionBar.tsx
@@ -7,11 +7,12 @@ import Button from './Button';
 export const PROPOSAL_DISTRIBUTION_COLORS = ['#304275', '#4864AF', '#7D92CA'];
 export const TURNOUTS_COLORS = ['#B7C3E1', '#7D92CA'];
 export const VOTE_DISTRIBUTION_COLORS = ['#7251B5', '#9163CB', '#B185DB'];
+export const DEFAULT_COLORS = ['#9E9E9E', '#B9B9B9', '#E4E4E4'];
 
 interface DistributionBarProps<T extends Percentage> {
   data: T[];
   renderContent?: (d: T) => ReactNode | ReactNode[] | null;
-  variant?: 'proposal' | 'vote' | 'turnouts';
+  variant?: 'proposal' | 'vote' | 'turnouts' | 'default';
 }
 const DistributionContainer = styled(ButtonGroup)(() => ({
   boxShadow: 'none',
@@ -30,26 +31,30 @@ const DistributionBar = <T extends Percentage>({
       colors = TURNOUTS_COLORS;
       break;
     case 'proposal':
-    default:
       colors = PROPOSAL_DISTRIBUTION_COLORS;
+      break;
+    default:
+      colors = DEFAULT_COLORS;
   }
   return (
     <DistributionContainer variant="contained" fullWidth>
       {map(data, (datum, idx) => {
         return (
-          <Button
-            key={`dist-${variant ?? ''}-${idx}`}
-            disabled
-            sx={{
-              backgroundColor: colors[idx],
-              display: 'block',
-              textAlign: 'left',
-              textTransform: 'none',
-              width: `${datum.percent}%`,
-            }}
-          >
-            {renderContent && renderContent(datum)}
-          </Button>
+          datum.percent > 0 && (
+            <Button
+              key={`dist-${variant ?? ''}-${idx}`}
+              disabled
+              sx={{
+                backgroundColor: colors[idx],
+                display: 'block',
+                textAlign: 'left',
+                textTransform: 'none',
+                width: `${datum.percent}%`,
+              }}
+            >
+              {renderContent && renderContent(datum)}
+            </Button>
+          )
         );
       })}
     </DistributionContainer>
