def7923e4d5882d6636cb15d6910b7b343374320
aeddaqqa <achrafeddaqqaq@gmail.com>
2024-07-26 08:52:08 +0100

    feat(webpack-config): split webpack config to prod and dev

---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { default: merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const publicPath = process.env.PUBLIC_PATH;
+
+module.exports = merge(common, {
+  mode: 'production',
+
+  output: {
+    publicPath: publicPath,
+    filename: 'js/[name].[fullhash:8].js',
+    chunkFilename: 'js/[name].[fullhash:8].js',
+  },
+
+  performance: {
+    hints: 'warning',
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+});
