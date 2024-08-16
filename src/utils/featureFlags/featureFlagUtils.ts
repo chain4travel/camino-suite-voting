40b8da44144bd341ed62c48fe23622ec74724221
Ysrbolles <ysrbolles@gmail.com>
2024-08-16 18:18:43 +0100

    feat: Add feature flag check for CairoPhase in isFeatureEnabled function

--- src/utils/featureFlags/featureFlagUtils.ts
+++ src/utils/featureFlags/featureFlagUtils.ts
@@ -1,21 +1,34 @@
-// src/utils/featureFlagUtils.ts
 import { useNetworkStore } from '@/store/network';
 
 export async function isFeatureEnabled(key: string): Promise<boolean> {
   const caminoClient = useNetworkStore(state => state.caminoClient);
 
   if (!caminoClient) {
-    console.warn(`Camino client is not available.`);
+    console.warn('Camino client is not available.');
     return false;
   }
 
-  const phase = await caminoClient.PChain().getUpgradePhases();
-  const { BerlinPhase } = phase;
+  const phases = await caminoClient.PChain().getUpgradePhases();
+  const { BerlinPhase, CairoPhase } = phases;
 
-  if (BerlinPhase && (isNaN(BerlinPhase) || BerlinPhase === 0)) {
+  if (!BerlinPhase || isNaN(BerlinPhase) || BerlinPhase === 0) {
     return false;
   }
 
-  const enabledFeatures = ['BaseFee', 'NewMember', 'ExcludeMember'];
-  return enabledFeatures.includes(key);
+  if (BerlinPhase >= 1) {
+    const berlinFeatures = ['NewMember', 'ExcludeMember'];
+
+    if (berlinFeatures.includes(key)) {
+      return true;
+    }
+  }
+
+  if (CairoPhase && !isNaN(CairoPhase) && CairoPhase >= 1) {
+    const cairoFeatures = ['BaseFee', 'FeeDistribution']; // 'FeeDistribution' is the key for distribution (30/30/40) of gas fee
+    if (cairoFeatures.includes(key)) {
+      return true;
+    }
+  }
+
+  return false;
 }
